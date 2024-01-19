f4fe956217354d3058706a0263ce542dca72780f
Amit M. Manthanwar <manthanwar@hotmail.com>
2024-01-19 12:34:26 +0100

    qgis python

---
+++ QGIS/Python/set-Symbology-Fill.py
@@ -0,0 +1,18 @@
+styles = QgsStyle.defaultStyle()
+#stylenames = styles.symbolNames()
+#for stylename in stylenames:
+#    print(stylename)
+
+style  = styles.symbol('gray 1 fill')
+
+#style  = styles.symbol('simple green line')
+#style  = styles.symbol('simple red line')
+#style  = styles.symbol('simple blue line')
+
+layers = iface.layerTreeView().selectedLayers()
+for layer in layers:
+    print(layer.name())
+    layer.renderer().setSymbol(style)
+    layer.triggerRepaint()
+    iface.layerTreeView().refreshLayerSymbology(layer.id())
+    
